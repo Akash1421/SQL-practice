e2994f94c7b796abdfaa7dd9197ec1b6511e0942
Akash1421 <akashreddy1421@gmail.com>
2025-12-05 12:44:25 +0530

    first commit

---
+++ PracticeQueries.sql
@@ -0,0 +1,18 @@
+---------------------------creating database syntax --------------------------------------------------
+
+--create database SalesDB;
+
+--Drop database SalesDB;
+
+--------------------------creating tables syntax ---------------------------------------------------
+
+--create table ProductPrices(product_id int, product_name varchar(100));
+
+--Insert into ProductPrices values(1,'Kevin');
+
+--Insert into ProductPrices values(2,'Tom'),(3,'Bigshow');
+
+--select * from SalesDB.dbo.ProductPrices;
+
+
+
